85ea79ef17754f82df09f9e5fdd929f3b7d4d98c
davromano <davidesmail00@gmail.com>
2024-04-25 20:13:48 +0200

    initial commit

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Example from './Example.js'; // Importing the AnotherComponent.js file
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Example /> {/* So here we can add components, parts of the website, visualizations like this */}
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/Example.js
@@ -0,0 +1,13 @@
+// Example.js
+
+import React from 'react';
+
+function Example() {
+  return (
+    <div>
+      <p>This is another component.</p>
+    </div>
+  );
+}
+
+export default Example;
