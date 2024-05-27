fcbf6a6f331d7b51f158ae84b1b13ed185bc9d00
Nil Y. Ilba <yagmurilba@gmail.com>
2024-05-27 13:43:37 +0200

    filtered_map_view

--- website/src/MapComponent.js
+++ website/src/MapComponent.js
@@ -1,67 +1,120 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import { geoMercator, geoPath } from 'd3-geo';
-import { json } from 'd3-fetch';
+import { csv } from 'd3-fetch';
+import dataUrl from './data/figure_2_choropleth.csv?url';
 
-const initialCountryData = {
-    "Austria": 30,
-    "Belgium": 25,
-    "Bulgaria": 40,
-    "Croatia": 20,
-    "Cyprus": 15,
-    "Czech Republic": 35,
-    "Denmark": 50,
-    "Estonia": 45,
-    "Finland": 55,
-    "France": 33,
-    "Germany": 48,
-    "Greece": 18,
-    "Hungary": 28,
-    "Ireland": 25,
-    "Italy": 40,
-    "Latvia": 22,
-    "Lithuania": 30,
-    "Luxembourg": 40,
-    "Malta": 10,
-    "Netherlands": 42,
-    "Poland": 38,
-    "Portugal": 45,
-    "Romania": 15,
-    "Slovakia": 27,
-    "Slovenia": 32,
-    "Spain": 37,
-    "Sweden": 50,
-    "United Kingdom": 35
-};
-
-// List of countries to be assigned the purplish color scale
-const menDominancyCountries = ["Germany", "France", "United Kingdom", "Italy", "Spain", "Poland"];
-
-// List of years for the timeline
 const years = [2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016, 2018, 2020];
-
+const categories = ['wellbeing_color', 'media_color', 'internet_color', 'relig_color', 'social_color', 'finstab_color'];
 const colors = {
-    media_consumption: "#F8AD1A",
-    religious_activity: "#F6810C",
-    financial_vulnerability: "#E34D20",
-    mental_stress: "#AA2243",
-    loneliness: "#6C0D59",
-    education: "#3F0059",
+    wellbeing_color: "#F8AD1A",
+    media_color: "#F6810C",
+    internet_color: "#E34D20",
+    relig_color: "#AA2243",
+    social_color: "#6C0D59",
+    finstab_color: "#3F0059",
 };
+const ageGroups = ["<25", "25-39", "40-59", "60+", "AGGREGATE"];
 
-const getRandomValues = () => {
-    const newValues = {};
-    Object.keys(initialCountryData).forEach(country => {
-        newValues[country] = Math.floor(Math.random() * 56); // Random values between 0 and 55
-    });
-    return newValues;
+const countryNameMapping = {
+    "Albania": "Albania",
+    "Austria": "Austria",
+    "Belgium": "Belgium",
+    "Bulgaria": "Bulgaria",
+    "Croatia": "Croatia",
+    "Cyprus": "Cyprus",
+    "Czechia": "Czech Republic",
+    "Denmark": "Denmark",
+    "Estonia": "Estonia",
+    "Finland": "Finland",
+    "France": "France",
+    "Germany": "Germany",
+    "Greece": "Greece",
+    "Hungary": "Hungary",
+    "Iceland": "Iceland",
+    "Ireland": "Ireland",
+    "Israel": "Israel",
+    "Italy": "Italy",
+    "Kosovo": "Kosovo",
+    "Latvia": "Latvia",
+    "Lithuania": "Lithuania",
+    "Luxembourg": "Luxembourg",
+    "Montenegro": "Montenegro",
+    "Netherlands": "Netherlands",
+    "North Macedonia": "Macedonia",
+    "Norway": "Norway",
+    "Poland": "Poland",
+    "Portugal": "Portugal",
+    "Romania": "Romania",
+    "Russian Federation": "Russia",
+    "Serbia": "Serbia",
+    "Slovakia": "Slovakia",
+    "Slovenia": "Slovenia",
+    "Spain": "Spain",
+    "Sweden": "Sweden",
+    "Switzerland": "Switzerland",
+    "Turkey": "Turkey",
+    "Ukraine": "Ukraine",
+    "United Kingdom": "United Kingdom",
+    "Bosnia and Herzegovina": "Bosnia and Herz.",
+    "Moldova": "Moldova",
+    "Tunisia": "Tunisia",
+    "Georgia": "Georgia",
+    "Armenia": "Armenia",
+    "Lebanon": "Lebanon"
 };
 
 const MapComponent = () => {
     const ref = useRef();
-    const [selectedYear, setSelectedYear] = useState(2020);
-    const [variable, setVariable] = useState('media_consumption');
-    const [countryData, setCountryData] = useState(initialCountryData);
+    const [selectedYear, setSelectedYear] = useState(2002);
+    const [variable, setVariable] = useState('wellbeing_color');
+    const [selectedAgeGroup, setSelectedAgeGroup] = useState('AGGREGATE');
+    const [data, setData] = useState(null);
+    const [filteredData, setFilteredData] = useState({});
+
+    useEffect(() => {
+        csv(dataUrl)
+            .then(csvData => {
+                console.log("CSV Data Loaded: ", csvData);
+                setData(csvData);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
+    useEffect(() => {
+        if (data !== null) {
+            const filtered = data.filter(d => +d.essround_yr === selectedYear && d.age_group === selectedAgeGroup);
+            console.log("Filtered Data: ", filtered);
+            const dataMap = {};
+            filtered.forEach(d => {
+                const countryName = countryNameMapping[d.country];
+                if (countryName) {
+                    dataMap[countryName] = {
+                        wellbeing_color: +d.wellbeing_color,
+                        wellbeing_men: +d.wellbeing_men,
+                        wellbeing_woman: +d.wellbeing_woman,
+                        media_color: +d.media_color,
+                        media_men: +d.media_men,
+                        media_woman: +d.media_woman,
+                        internet_color: +d.internet_color,
+                        internet_men: +d.internet_men,
+                        internet_woman: +d.internet_woman,
+                        relig_color: +d.relig_color,
+                        relig_men: +d.relig_men,
+                        relig_woman: +d.relig_woman,
+                        social_color: +d.social_color,
+                        social_men: +d.social_men,
+                        social_woman: +d.social_woman,
+                        finstab_color: +d.finstab_color,
+                        finstab_men: +d.finstab_men,
+                        finstab_woman: +d.finstab_woman,
+                    };
+                }
+            });
+            console.log("Data Map: ", dataMap);
+            setFilteredData(dataMap);
+        }
+    }, [selectedYear, selectedAgeGroup, variable, data]);
 
     useEffect(() => {
         const svg = d3.select(ref.current)
@@ -71,17 +124,14 @@ const MapComponent = () => {
         const g = svg.append('g');
 
         const projection = geoMercator()
-            .center([20, 52]) // Approximate center of Europe
-            .scale(800) // Adjusted scale for zoom in
-            .translate([600, 350]); // Adjusted translate for right shift
+            .center([20, 52])
+            .scale(800)
+            .translate([600, 350]);
 
         const path = geoPath().projection(projection);
 
-        const womenColorScale = d3.scaleSequential(d3.interpolateOranges)
-            .domain([0, 55]); // Adjust the domain based on your data range
-
-        const menColorScale = d3.scaleSequential(d3.interpolatePurples)
-            .domain([0, 55]); // Adjust the domain based on your data range
+        const womenColorScale = d3.scaleSequential(d3.interpolateOranges).domain([1, 7]);
+        const menColorScale = d3.scaleSequential(d3.interpolatePurples).domain([8, 14]);
 
         const tooltip = d3.select('body').append('div')
             .attr('class', 'tooltip')
@@ -93,9 +143,9 @@ const MapComponent = () => {
             .style('border-radius', '4px')
             .style('box-shadow', '0 0 10px rgba(0, 0, 0, 0.5)');
 
-        json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(data => {
+        d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(geoData => {
             const paths = g.selectAll('path')
-                .data(data.features)
+                .data(geoData.features)
                 .enter().append('path')
                 .attr('d', path)
                 .attr('stroke', 'white')
@@ -103,27 +153,25 @@ const MapComponent = () => {
                 .on('mouseover', function(event, d) {
                     d3.select(this).attr('fill', '#6e6e6e');
                     const countryName = d.properties.name;
-                    const value = countryData[countryName];
-                    tooltip.html(`<strong>${countryName}</strong><br>Value: ${value}`)
-                        .style('visibility', 'visible');
+                    const countryData = filteredData[countryName];
+                    if (countryData) {
+                        tooltip.html(`<strong>${countryName}</strong><br>
+                            ${variable.replace('_color', ' Men')}: ${countryData[variable.replace('color', 'men')]}<br>
+                            ${variable.replace('_color', ' Women')}: ${countryData[variable.replace('color', 'woman')]}`)
+                            .style('visibility', 'visible');
+                    }
                 })
                 .on('mousemove', function(event) {
                     tooltip.style('top', (event.pageY - 10) + 'px')
                         .style('left', (event.pageX + 10) + 'px');
                 })
                 .on('mouseout', function(event, d) {
-                    const countryName = d.properties.name;
-                    const value = countryData[countryName];
-                    if (menDominancyCountries.includes(countryName)) {
-                        d3.select(this).attr('fill', value ? menColorScale(value) : '#cccccc');
-                    } else {
-                        d3.select(this).attr('fill', value ? womenColorScale(value) : '#cccccc');
-                    }
+                    updateMapColors(); 
                     tooltip.style('visibility', 'hidden');
                 });
 
             g.selectAll('text')
-                .data(data.features)
+                .data(geoData.features)
                 .enter()
                 .append('text')
                 .attr('x', d => path.centroid(d)[0])
@@ -134,25 +182,23 @@ const MapComponent = () => {
                 .style('font-size', 11)
                 .style('fill', 'black');
 
-            // Add legend frame
             const legendFrame = svg.append('g')
-                .attr('transform', 'translate(730, 600)') // Position at bottom-right corner
+                .attr('transform', 'translate(730, 600)')
                 .attr('class', 'legend-frame');
 
             legendFrame.append('rect')
                 .attr('width', 250)
                 .attr('height', 90)
-                .attr('fill', '#f0f0f0') // Light grey background
+                .attr('fill', '#f0f0f0')
                 .attr('stroke', 'black')
                 .attr('stroke-width', 1.5);
 
-            // Add legend inside frame
             const legend = legendFrame.append('g')
                 .attr('transform', 'translate(10, 10)');
 
             const legendData = [
-                { label: 'Women Dominancy', colors: [ '#fff5eb', '#fee6ce', '#fdd0a2', '#fdae6b', '#fd8d3c', '#e6550d', '#a63603'] }, // Adjust colors to match your scale
-                { label: 'Men Dominancy', colors: [ '#fcfbfd', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#756bb1', '#54278f'] }   // Adjust colors to match your scale
+                { label: 'Women', colors: ['#fff5eb', '#fee6ce', '#fdd0a2', '#fdae6b', '#fd8d3c', '#e6550d', '#a63603'] },
+                { label: 'Men', colors: ['#fcfbfd', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#756bb1', '#54278f'] }
             ];
 
             legend.selectAll('g')
@@ -183,118 +229,65 @@ const MapComponent = () => {
             const updateMapColors = () => {
                 paths.attr('fill', d => {
                     const countryName = d.properties.name;
-                    const value = countryData[countryName];
-                    if (menDominancyCountries.includes(countryName)) {
-                        return value ? menColorScale(value) : '#cccccc';
-                    } else {
-                        return value ? womenColorScale(value) : '#cccccc';
+                    const countryData = filteredData[countryName];
+                    if (countryData) {
+                        const colorValue = countryData[variable];
+                        if (colorValue >= 1 && colorValue <= 7) {
+                            return womenColorScale(colorValue);
+                        } else if (colorValue >= 8 && colorValue <= 14) {
+                            return menColorScale(colorValue);
+                        }
                     }
+                    return '#cccccc'; // Default color for countries without data
                 });
             };
 
             updateMapColors();
         });
 
-    }, [selectedYear]);
-
-    useEffect(() => {
-        const svg = d3.select("#timeline-svg");
-        svg.selectAll("*").remove(); // Clear previous content
-
-        const timeline = svg.append('g')
-            .attr('transform', 'translate(50, 25)'); // Adjusted position
-
-        // Add timeline line
-        timeline.append('line')
-            .attr('x1', 0)
-            .attr('y1', 10)
-            .attr('x2', 800)
-            .attr('y2', 10)
-            .attr('stroke', 'black')
-            .attr('stroke-width', 2);
-
-        // Add years to the timeline
-        const yearGroups = timeline.selectAll('g.year-group')
-            .data(years)
-            .enter()
-            .append('g')
-            .attr('class', 'year-group')
-            .attr('transform', (d, i) => `translate(${i * 80 + 40}, 0)`)
-            .on('click', d => {
-                setSelectedYear(d);
-                setCountryData(getRandomValues());
-            });
-
-        yearGroups.append('text')
-            .text(d => d)
-            .attr('y', -10)
-            .attr('text-anchor', 'middle')
-            .style('font-size', 14)
-            .style('font-weight', 'bold')
-            .style('fill', 'black')
-            .style('cursor', 'pointer');
-
-        yearGroups.append('line')
-            .attr('x1', 0)
-            .attr('y1', 10)
-            .attr('x2', 0)
-            .attr('y2', 20)
-            .attr('stroke', 'black')
-            .attr('stroke-width', 2);
-
-        // Add timeline handle
-        const handle = timeline.append('circle')
-            .attr('cx', 40 + (years.indexOf(selectedYear) * 80))
-            .attr('cy', 10)
-            .attr('r', 8)
-            .attr('fill', 'black')
-            .call(d3.drag().on('drag', function(event) {
-                const yearIndex = Math.max(0, Math.min(9, Math.round((event.x - 40) / 80)));
-                setSelectedYear(years[yearIndex]);
-                setCountryData(getRandomValues());
-                handle.attr('cx', 40 + yearIndex * 80);
-            }));
-
-        // Update the handle position when the selected year changes
-        handle.attr('cx', 40 + (years.indexOf(selectedYear) * 80));
-    }, [selectedYear]);
-
-    useEffect(() => {
-        const svg = d3.select(ref.current);
-        const womenColorScale = d3.scaleSequential(d3.interpolateOranges).domain([0, 55]);
-        const menColorScale = d3.scaleSequential(d3.interpolatePurples).domain([0, 55]);
-
-        // Update map colors whenever the country data changes
-        svg.selectAll('path')
-            .attr('fill', d => {
-                const countryName = d.properties.name;
-                const value = countryData[countryName];
-                if (menDominancyCountries.includes(countryName)) {
-                    return value ? menColorScale(value) : '#cccccc';
-                } else {
-                    return value ? womenColorScale(value) : '#cccccc';
-                }
-            });
-    }, [countryData]);
+    }, [selectedYear, selectedAgeGroup, variable, filteredData]);
 
     return (
         <div>
             <div className="flex gap-x-3 mb-4">
-                {Object.keys(colors).map((varName) => (
+                {categories.map((category) => (
                     <button
-                        key={varName}
-                        style={{ backgroundColor: `${colors[varName]}`, color: "white" }}
+                        key={category}
+                        style={{ backgroundColor: `${colors[category]}`, color: "white" }}
                         className="text-white font-bold py-2 px-4 rounded"
-                        onClick={() => setVariable(varName)}
+                        onClick={() => setVariable(category)}
                     >
-                        {varName.replace("_", " ")}
+                        {category.replace("_color", "").replace("_", " ")}
+                    </button>
+                ))}
+            </div>
+            <div className="flex gap-x-3 mb-4">
+                <select
+                    onChange={(e) => setSelectedAgeGroup(e.target.value)}
+                    value={selectedAgeGroup}
+                    className="text-white font-bold py-2 px-4 rounded bg-gray-600"
+                >
+                    {ageGroups.map(ageGroup => (
+                        <option key={ageGroup} value={ageGroup}>{ageGroup}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="flex gap-x-3 mb-4">
+                {years.map(year => (
+                    <button
+                        key={year}
+                        style={{
+                            backgroundColor: selectedYear === year ? 'blue' : 'gray',
+                            color: 'white'
+                        }}
+                        className="text-white font-bold py-2 px-4 rounded"
+                        onClick={() => setSelectedYear(year)}
+                    >
+                        {year}
                     </button>
                 ))}
             </div>
             <svg ref={ref} />
-            <div className="flex justify-center mt-4">
-                <svg id="timeline-svg" width="900" height="50"></svg>
-            </div>
         </div>
     );
 }
