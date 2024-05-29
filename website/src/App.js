5007d2e52d59d16f0f702abe2c9caa31678f9486
Nil Y. Ilba <yagmurilba@gmail.com>
2024-05-29 10:07:58 +0200

    allignment

--- website/src/App.js
+++ website/src/App.js
@@ -35,43 +35,44 @@ function App() {
         </div>
       </header>
 
-      <section>
-        <h2 className="text-3xl w-96 ml-7 mt-5 flex justify-center">
+
+      <section className="section">
+        <h2 className="text-3xl w-96 ml-7 mt-5">
           1. Bar Chart: Setting the Stage
         </h2>
-        <p className="text-base w-3/4 mt-10 text-left ml-7 flex justify-center">
+        <p className="text-base w-3/4 mt-10 text-container">
           Our journey starts with a Europe-wide snapshot. This bar chart shows
           average levels of a chosen variable (like trust in institutions) for
           four different age groups over time. This helps you get a feel for
           overall trends before we explore how these trends differ for men and
           women.
         </p>
-        <div className="component-container flex justify-center">
+        <div className="component-container">
           <BarChart variable={variable} />
         </div>
       </section>
 
-      <section>
-        <h2 className="text-3xl w-96 text-left ml-7 mt-5">
+      <section className="section">
+        <h2 className="text-3xl w-96 ml-7 mt-5">
           2. Gender Divide Across Europe
         </h2>
-        <p className="text-base w-3/4 mt-10 text-left ml-7">
+        <p className="text-base w-3/4 mt-10 text-container">
           Next, we zoom in to see how men and women compare across Europe. This
           heatmap uses color to show where women or men report higher levels of a
           chosen variable, like political participation. You can select an age
           group and drag the year slider to see how these differences change over
           time. Hover over a country to see a detailed breakdown by year.
         </p>
-        <div id="map" className="flex justify-center items-center mt-4">
+        <div id="map" className="component-container">
           <MapComponent variable={variable}/>
         </div>
       </section>
 
-      <section>
-        <h2 className="text-3xl w-96 text-left ml-7 mt-5">
+      <section className="section">
+        <h2 className="text-3xl w-96 ml-7 mt-5">
           3. Ranking the Gender Divide
         </h2>
-        <p className="text-base w-3/4 mt-10 text-left ml-7">
+        <p className="text-base w-3/4 mt-10 text-container">
           This final view lets you compare countries side-by-side. It shows a
           ranked list of European nations based on the chosen variable's gender
           gap in a specific year. Hover over a country to see the exact numbers
